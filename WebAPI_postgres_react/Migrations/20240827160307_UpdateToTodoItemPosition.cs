9039bfd213c2585d223cb9d3044e030680153c91
Sindre Sauarlia <101213882+SindreSau@users.noreply.github.com>
2024-08-27 18:32:55 +0200

    Updated positions migrations

---
+++ WebAPI_postgres_react/Migrations/20240827160307_UpdateToTodoItemPosition.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPI_postgres_react.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateToTodoItemPosition : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Position",
+                table: "TodoItems",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Position",
+                table: "TodoItems");
+        }
+    }
+}
